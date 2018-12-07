4de937aafca19439645ef1648d692e0a634056be
mrcxt <847428891@qq.com>
2018-12-07 14:35:34 +0800

    add canvas

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header>
+        <el-menu mode="horizontal" router>
+          <template v-for="(item, index) in menuList">
+            <template v-if="item.meta.visible">
+              <el-menu-item v-if="!item.children" :key="item.key" :index="item.path">{{item.name}}</el-menu-item>
+              <sub-menu v-else :data="item" :path="item.path" :key="item.key"></sub-menu>
+            </template>
+          </template>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
   </div>
 </template>
 
+<script>
+import SubMenu from "./components/subMenu";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    "sub-menu": SubMenu
+  },
+  computed: {
+    menuList() {
+      return this.$router.options.routes;
+    }
+  },
+  created() {
+    console.log(this.menuList);
+  },
+  methods: {}
+};
+</script>
+
+
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
